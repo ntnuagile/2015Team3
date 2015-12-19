9784b4c46b1f795b8187ad087e5b173c090fb0b1
Special233 <lv05407@yahoo.com.tw>
2015-12-20 03:53:04 +0800

    Category加入

--- TeamProject/Category.cs
+++ TeamProject/Category.cs
@@ -8,5 +8,24 @@ namespace TeamProject
 {
     class Category
     {
+        public ArticleDataBase[] CategoryList = new ArticleDataBase[100];
+        public int MaxCategory = 100;
+        public int NumCategory = 0;
+
+        public void AddCategory(ArticleDataBase db)
+        {
+            CategoryList[NumCategory] = db;
+            NumCategory += 1;
+        }
+
+        public void Print()
+        {
+            for(int i=0;i<NumCategory;i+=1)
+            {
+                Console.WriteLine("<{0}> {1}", i + 1, CategoryList[i].name);
+            }
+        }
     }
+
+    
 }
