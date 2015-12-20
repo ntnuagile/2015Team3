6006f32d6c8569c93b2020d09288f4e94b277173
煞氣a明彰 <moto84x@yahoo.com.tw>
2015-12-20 16:30:15 +0800

    抱歉!!這次USER改了很多!!
    在拿資料的時候我曾了Get的function
    EX:Account->GetAccount

--- TeamProject/User.cs
+++ TeamProject/User.cs
@@ -8,49 +8,91 @@ namespace TeamProject
 {
     class User
     {
-        public string Username { get; set; }//姓名
-        public string Password { get; set; }
+        //姓名
+        private string Username { get; set; }
+        public string GetUser()
+        {
+            return Username;
+        }
+        public void SetUser(string name)
+        {
+            Username = name;
+        }
 
-        
-        public string Name { get; set; }//暱稱
-
+        //暱稱
+        private string Name { get; set; }
         internal void SetName(string account_name)
         {
             Name = account_name;
         }
+        public string GetName()
+        {
+            return Name;
+        }
 
-        public string Account { get; set; }
 
-        internal void SetAccount(string account_name)
+        //帳號
+        private string Account { get; set; }
+        public void SetAccount(string account_name)
         {
             Account = account_name;
         }
+        public string GetAccount()
+        {
+            return Account;
+        }
 
-        public char Gender { get; set; }
-        internal void SetGender(char gender_kind)
+
+        //性別
+        private char Gender { get; set; }
+        public void SetGender(char gender_kind)
         {
             Gender = gender_kind;
         }
+        public char GetGender()
+        {
+            return Gender;
+        }
 
-        internal void SetPassword(string password)
+
+        //密碼
+        private string Password { get; set; }
+        public void SetPassword(string password)
         {
             Password = password;
         }
-        public string Birth { get; set; }
-        internal void SetBirth(string birth)
+        public string GetPassword()
+        {
+            return Password;
+        }
+
+
+        //生日
+        private string Birth { get; set; }
+        public void SetBirth(string birth)
         {
             Birth = birth;
         }
-        public string ID { get; set; }
-        internal void SetID(string id_number)
+        public string GetBirth()
+        {
+            return Birth;
+        }
+
+
+        //身分證字號
+        private string ID { get; set; }
+        public void SetID(string id_number)
         {
             ID = id_number;
         }
-        public bool IsLogin { get; set; }
-
-        internal string GetAccount()
+        public string GetID()
         {
-            throw new NotImplementedException();
+            return ID;
         }
+
+
+        //登入狀態
+        public bool IsLogin { get; private set; }
+
     }
 }
